609802ec31b7212fe9a3ecd12b7e31e4a5750f3f
Greg J <39595823+OfficialGregJ@users.noreply.github.com>
2024-09-18 22:12:33 -0700

    Basic 3tier architecture
    Very basic 3 tier implementation to follow scheduling as given by prof

---
+++ src/main/java/RegisterDao.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// Change dburl once name finalized, change dbpw => .env??
+
+public class RegisterDao {
+	private String dburl="jdbc:mysql://localhost:3306/???";
+	private String dbuname="root";
+	private String dbpassword="";
+	private String dbdriver="com.mysql.jdbc.Driver";
+	
+	public void loadDriver(String dbDriver) {
+		try {
+			Class.forName(dbDriver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() {
+		Connection con=null;
+		try {
+			con = DriverManager.getConnection(dburl, dbuname, dbpassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public String insert(Member member) {
+		loadDriver(dbdriver);
+		Connection con = getConnection();
+		String result = "data entered successfully";
+		String sql = "insert into member values(?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, member.getUname());
+			ps.setString(2, member.getPassword());
+			ps.setString(3, member.getEmail());
+			ps.setString(4, member.getPhone());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = "data not entered";
+		}
+		
+		return result;	
+	}
+}
