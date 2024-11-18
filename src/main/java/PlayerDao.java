ab14e48aa584d1f7c6f9d34b2dbe4454cb9aa2a5
Timothy Kim <timothy.e.kim@sjsu.edu>
2024-11-18 11:55:46 -0800

    added missing code

---
+++ src/main/java/PlayerDao.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerDao {
+    private String dburl = "jdbc:mysql://localhost:3306/deep-drive";
+    private String dbuname = "root";
+    private String dbpassword = "";
+    private String dbdriver = "com.mysql.cj.jdbc.Driver";
+    
+    public List<String> getAllPlayerNames() {
+        List<String> playerNames = new ArrayList<>();
+        
+        try {
+            Class.forName(dbdriver);
+            try (Connection con = DriverManager.getConnection(dburl, dbuname, dbpassword);
+                 Statement stmt = con.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT Name FROM player")) {
+                
+                while (rs.next()) {
+                    playerNames.add(rs.getString("Name"));
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return playerNames;
+    }
+}
