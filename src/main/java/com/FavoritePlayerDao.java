c1c9106b6861e68cc7fb896ea54524105461734b
Benny Ngo <bkvngo@gmail.com>
2024-12-01 22:02:26 -0800

    Favorite players now displays

--- src/main/java/com/FavoritePlayerDao.java
+++ src/main/java/com/FavoritePlayerDao.java
@@ -40,14 +40,14 @@ public class FavoritePlayerDao {
 
     public List<String> getFavoritePlayers(String username) {
         List<String> favoritePlayers = new ArrayList<>();
-        String query = "SELECT Player FROM `deep-drive`.favorites WHERE Username = ?";
+        String query = "SELECT Players FROM `deep-drive`.favorites WHERE User = ?";
 
         try (Connection conn = getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, username);
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
-                    favoritePlayers.add(rs.getString("Player"));
+                    favoritePlayers.add(rs.getString("Players"));
                 }
             }
         } catch (SQLException e) {
@@ -58,7 +58,7 @@ public class FavoritePlayerDao {
     }
 
     public void addFavoritePlayer(String username, String playerName) throws SQLException {
-        String query = "INSERT INTO `deep-drive`.favorites (Username, Player) VALUES (?, ?)";
+        String query = "INSERT INTO `deep-drive`.favorites (User, Players) VALUES (?, ?)";
         try (Connection conn = getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, username);
