69e7ac310a307837769cd4c8eaf5ee68ee005c34
Benny Ngo <bkvngo@gmail.com>
2024-12-16 00:19:34 -0800

    Added "Include Create Schema" for new users

--- Dump20241014/deep-drive_player_game_stats.sql
+++ Dump20241014/deep-drive_player_game_stats.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `deep-drive` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `deep-drive`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: deep-drive
@@ -55,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-03  0:48:30
+-- Dump completed on 2024-12-16  0:16:55
