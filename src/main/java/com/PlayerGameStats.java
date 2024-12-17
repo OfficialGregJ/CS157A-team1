0c39604eefb2981a3c53e9f81cd4136cf4cc567c
Benny Ngo <bkvngo@gmail.com>
2024-12-16 16:24:23 -0800

    Added missing files for Player game stats

---
+++ src/main/java/com/PlayerGameStats.java
@@ -0,0 +1,105 @@
+package com;
+
+public class PlayerGameStats{
+	private String playerName;
+    private String team;
+    private int points;
+    private int rebounds;
+    private int assists;
+    private int blocks;
+    private double freeThrowPercentage;
+    private double threePointPercentage;
+    private int turnovers;
+
+    // Constructor
+    public PlayerGameStats() {
+    }
+
+    // Getters and setters for all fields
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getRebounds() {
+        return rebounds;
+    }
+
+    public void setRebounds(int rebounds) {
+        this.rebounds = rebounds;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(int blocks) {
+        this.blocks = blocks;
+    }
+
+    public double getFreeThrowPercentage() {
+        return freeThrowPercentage;
+    }
+
+    public void setFreeThrowPercentage(double freeThrowPercentage) {
+        this.freeThrowPercentage = freeThrowPercentage;
+    }
+
+    public double getThreePointPercentage() {
+        return threePointPercentage;
+    }
+
+    public void setThreePointPercentage(double threePointPercentage) {
+        this.threePointPercentage = threePointPercentage;
+    }
+
+    public int getTurnovers() {
+        return turnovers;
+    }
+
+    public void setTurnovers(int turnovers) {
+        this.turnovers = turnovers;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerStats{" +
+                "playerName='" + playerName + '\'' +
+                ", team='" + team + '\'' +
+                ", points=" + points +
+                ", rebounds=" + rebounds +
+                ", assists=" + assists +
+                ", blocks=" + blocks +
+                ", freeThrowPercentage=" + freeThrowPercentage +
+                ", threePointPercentage=" + threePointPercentage +
+                ", turnovers=" + turnovers +
+                '}';
+    }
+}
